f313c5c1d3b857cc37dd2641e9a0cd1de275abda
cs-pedro-silveira <pedro.silveira@accenture.com>
2021-12-18 11:30:45 -0300

    Add URLRequestBuilder

---
+++ MobileChallengeLuizaLabs/Core/Core/Network Client/URLRequestBuilder/URLRequestBuilder.swift
@@ -0,0 +1,65 @@
+//
+//  URLRequestBuilder.swift
+//  Core
+//
+//  Created by pedro.silveira on 18/12/21.
+//
+
+import Foundation
+
+struct URLRequestBuilder {
+
+    private let request: HTTPRequest
+
+    private var httpMethod: HTTPMethod {
+        request.method
+    }
+
+    private var headers: [String: String]? {
+        request.headers
+    }
+
+    private var bodyData: Data? {
+        try? request.body?.getData()
+    }
+
+    private var queryParameters: [String: String]? {
+        request.queryParameters
+    }
+
+    private func createRequest(url: URL, method: HTTPMethod) -> NSMutableURLRequest {
+        let request = NSMutableURLRequest(url: url)
+
+        request.httpMethod = method.rawValue
+
+        return request
+    }
+
+    func createRequest() -> NSMutableURLRequest? {
+        var urlComponents = request.urlComponents
+
+        if let queryParameters = queryParameters {
+            urlComponents.queryItems = queryParameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+        }
+
+        guard let url = urlComponents.url else {
+            return nil
+        }
+
+        let urlRequest = createRequest(url: url, method: httpMethod)
+
+        if let headers = headers {
+            headers.forEach {
+                urlRequest.addValue($0.value, forHTTPHeaderField: $0.key)
+            }
+        }
+
+        urlRequest.httpBody = bodyData
+
+        return urlRequest
+    }
+
+    init(from request: HTTPRequest) {
+        self.request = request
+    }
+}
