5c200e7568c5e308f739576655c2f8d9db9c3f8f
cs-pedro-silveira <pedro.silveira@accenture.com>
2021-12-19 14:31:59 -0300

    Add PullRequestListTableViewCellViewModel

---
+++ MobileChallengeLuizaLabs/Repository/Repository/Screens/PullRequestList/View/Subviews/Cell/PullRequestListTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  PullRequestListTableViewCell.swift
+//  Repository
+//
+//  Created by pedro.silveira on 19/12/21.
+//
+
+import Foundation
---
+++ MobileChallengeLuizaLabs/Repository/Repository/Screens/PullRequestList/View/Subviews/Cell/ViewModel/PullRequestListTableViewCellViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  PullRequestListTableViewCellViewModel.swift
+//  Repository
+//
+//  Created by pedro.silveira on 19/12/21.
+//
+
+import UIKit
+
+struct PullRequestListTableViewCellViewModel: Hashable {
+
+    public var pullRequestTitle: String
+
+    public var pullRequestDescription: String
+
+    public var userName: String
+
+    public var profileImage: UIImage
+}
