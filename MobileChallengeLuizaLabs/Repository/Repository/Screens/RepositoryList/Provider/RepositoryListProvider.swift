16690a7d36e13a490e980f21ef2d0cd18566b562
cs-pedro-silveira <pedro.silveira@accenture.com>
2021-12-19 11:41:58 -0300

    Add RepositoryListProvider

---
+++ MobileChallengeLuizaLabs/Repository/Repository/Screens/RepositoryList/Provider/RepositoryListProvider.swift
@@ -0,0 +1,21 @@
+//
+//  RepositoryListProvider.swift
+//  Repository
+//
+//  Created by pedro.silveira on 19/12/21.
+//
+
+import Core
+
+struct RepositoryListProvider: RepositoryListProviderProtocol {
+
+    private let service: RepositoryServiceProtocol
+
+    init(service: RepositoryServiceProtocol = RepositoryService()) {
+        self.service = service
+    }
+
+    func fetchRepositories(currentPage: Int, _ completion: @escaping (Result<RepositoyListResponse, NetworkError>) -> Void) {
+        service.fetchRepositories(currentPage: currentPage, completion)
+    }
+}
